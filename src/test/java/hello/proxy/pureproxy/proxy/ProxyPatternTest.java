3b46d42a0163bb340e3947aac8b57c2ab2beea27
JunSeo Kwak <Ruggellia@google.com>
2022-05-16 02:08:14 +0900

    Proxy Pattern

---
+++ src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -0,0 +1,32 @@
+package hello.proxy.pureproxy.proxy;
+
+import hello.proxy.pureproxy.proxy.code.CacheProxy;
+import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
+import hello.proxy.pureproxy.proxy.code.RealSubject;
+import hello.proxy.pureproxy.proxy.code.Subject;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ProxyPatternTest {
+
+    @Test
+    void noProxyTest(){
+        RealSubject subject = new RealSubject();
+        ProxyPatternClient client = new ProxyPatternClient(subject);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+    @Test
+    void proxyTest(){
+        CacheProxy cacheProxy = new CacheProxy(new RealSubject());
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
+
+        client.execute();
+        client.execute();
+        client.execute();
+
+    }
+}
